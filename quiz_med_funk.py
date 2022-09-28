ae1fc66835e5c23af2f5cea2f7d7c156f8a828a4
Björn Larsson <larsson830@gmail.com>
2022-09-28 10:18:01 +0200

    Flyttat ut frågor till egen fil för att få det mer överskådligt

--- quiz_med_funk.py
+++ quiz_med_funk.py
@@ -1,23 +1,5 @@
 
-# Själva quizet, en dict med index. DVS index = siffrorna innan
-quiz = {
-    1 :{
-        "fråga" : "Vilket lag är bäst i Stockholm?",
-        "svar" : "hammarby",
-        "alternativ": "hammarby, djurgården eller aik"
-    },
-    2 :{
-        "fråga" : "Vad heter portugals huvudstad?",
-        "svar" : "lissabon",
-        "alternativ" : "lissabon, porto eller coimbra"
-    },
-    3 :{
-        "fråga" : "Vad kallas en sälunge?",
-        "svar" : "kut",
-        "alternativ" : "kut, balle eller fjalle"
-    }
-}
-
+from quiz import quiz
 # POÄNG räkning. måste kanske ligga i loop?
 score = 0
 # score += 1 , efter rätt svar
